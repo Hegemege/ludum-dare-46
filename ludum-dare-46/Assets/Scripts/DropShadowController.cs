5d33df2dea73442387358d2bcacedae8fb90b7f1
Henri Niva <henri.a.niva@gmail.com>
2020-04-19 19:41:56 +0300

    Add drop shadow

---
+++ ludum-dare-46/Assets/Scripts/DropShadowController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityUtilities;
+
+public class DropShadowController : MonoBehaviour
+{
+    public PlayerController PlayerController;
+
+    private Vector3 _startPosition;
+
+    void Awake()
+    {
+        _startPosition = transform.position;
+        TrackPlayer();
+    }
+
+    void FixedUpdate()
+    {
+        // Always position the dropshadow below the player
+        TrackPlayer();
+    }
+
+    private void TrackPlayer()
+    {
+        transform.position = new Vector3(PlayerController.transform.position.x, _startPosition.y, PlayerController.transform.position.z);
+        // Set scale based on height
+        var scaleT = Mathf.Clamp(PlayerController.transform.position.y / 5f, 0f, 1f);
+        var scale = Mathf.Lerp(1f, 0.25f, scaleT);
+        transform.localScale = Vector3.one * scale;
+    }
+}
